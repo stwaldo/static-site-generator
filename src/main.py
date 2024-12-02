9d61c031da797344f0ec373b281ad39262dcaa7a
stwaldo <stwaldo@gmail.com>
2024-12-02 15:53:46 -0500

    Remove public folder and its contents; add static CSS file and update main.py to copy static files to public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
+import shutil
 from node_types.textnode import TextNode
 
 def main():
-    text_node = TextNode("This is a TextNode", "bold", "https://example.com")
-    print(text_node)
+    copy_static_to_public()
+    
+def copy_static_to_public():
+    # Delete contents of the `public` folder
+    shutil.rmtree('public') 
+    # recursively copy the contents of the `static` folder to the `public` folder
+    shutil.copytree('static', 'public')
 
 if __name__ == "__main__":
     main()
