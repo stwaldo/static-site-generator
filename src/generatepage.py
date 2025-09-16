9124269e0b7d280d9b6c3f517b6579558243c91b
Adam Grow <adam.c.grow@gmail.com>
2025-09-15 22:08:24 -0400

    Refactor page generation to support base path and update main script to copy static files to docs folder

--- src/generatepage.py
+++ src/generatepage.py
@@ -3,7 +3,7 @@ from extracttitle import extract_title
 from markdowntohtmlnode import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print("Generating page from {} to {} using {}", from_path, dest_path, template_path)
    
     with open(from_path, 'r') as f:
@@ -17,11 +17,13 @@ def generate_page(from_path, template_path, dest_path):
     
     template = template.replace('{{ Title }}', title)
     template = template.replace('{{ Content }}', html)
-    
+    template = template.replace('href="/', f'href="{ basepath }')
+    template = template.replace('src="/', f'src="{ basepath }')
+
     with open(dest_path, 'w') as f:
         f.write(template)
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     # crawl every entry in the `dir_path_content` directory
     # for each markdown file, generate a new `.html` file using the same template, written to the `dest_dir_path`
     # directory structure from `dir_path_content` should be maintained in `dest_dir_path`
@@ -33,9 +35,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         
         if os.path.isdir(entry_path):
             os.makedirs(dest_entry_path, exist_ok=True)
-            generate_pages_recursive(entry_path, template_path, dest_entry_path)
+            generate_pages_recursive(entry_path, template_path, dest_entry_path, basepath)
         elif entry.endswith('.md'):
             dest_entry_path = os.path.splitext(dest_entry_path)[0] + '.html'
-            generate_page(entry_path, template_path, dest_entry_path)
+            generate_page(entry_path, template_path, dest_entry_path, basepath)
         else:
             os.makedirs(dest_entry_path, exist_ok=True)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 import shutil
+import sys
+import os
 from generatepage import generate_pages_recursive
-from node_types.textnode import TextNode
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
     copy_static_to_public()
-    generate_pages_recursive('content', 'template.html', 'public')
-    
+    generate_pages_recursive('content', 'template.html', 'docs', basepath)
 def copy_static_to_public():
-    # Delete contents of the `public` folder
-    shutil.rmtree('public') 
-    # recursively copy the contents of the `static` folder to the `public` folder
-    shutil.copytree('static', 'public')
+    # Delete contents of the `docs` folder
+    if os.path.exists('docs'):
+        shutil.rmtree('docs')
+    # recursively copy the contents of the `static` folder to the `docs` folder
+    shutil.copytree('static', 'docs')
 
 if __name__ == "__main__":
     main()
